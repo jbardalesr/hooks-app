fa708b0e8c34f7228635beb353bf44e8c2719f0f
Jordi Bardales <jbardalesr@uni.pe>
2021-03-28 11:45:39 -0500

    [add] react-helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import '../styles/components/Checkout.css';
 import { Link } from 'react-router-dom';
 import AppContext from '../context/AppContext';
+import { Helmet } from 'react-helmet';
 
 const Checkout = () => {
   // traeremos dos elementos del del contexto
@@ -20,33 +21,39 @@ const Checkout = () => {
   };
 
   return (
-    <div className="checkout">
-      <div className="checkout-content">
-        {/** cuando hay pedidos o no hay */}
-        {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos</h3>}
-        {/** recorremos todos los items del cart */}
-        {cart.map((item) => (
-          <div className="checkout-item">
-            <div className="checkout-element">
-              <h4>{item.title}</h4>
-              <span>${item.price}</span>
+    <>
+      <Helmet>
+        <title>Lista de pedidos _ Hooks Conf Merch</title>
+      </Helmet>
+
+      <div className="checkout">
+        <div className="checkout-content">
+          {/** cuando hay pedidos o no hay */}
+          {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos</h3>}
+          {/** recorremos todos los items del cart */}
+          {cart.map((item) => (
+            <div className="checkout-item">
+              <div className="checkout-element">
+                <h4>{item.title}</h4>
+                <span>${item.price}</span>
+              </div>
+              <button type="button" onClick={() => handleRemove(item)}>
+                <i className="fas fa-trash-alt"></i>
+              </button>
             </div>
-            <button type="button" onClick={() => handleRemove(item)}>
-              <i className="fas fa-trash-alt"></i>
-            </button>
-          </div>
-        ))}
-      </div>
-      {/** mostramos el precio total si el hay contenido. Si es mayor que 0 lo mostramos con &&*/}
-      {cart.length > 0 && (
-        <div className="checkout-sidebar">
-          <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continuar pedido</button>
-          </Link>
+          ))}
         </div>
-      )}
-    </div>
+        {/** mostramos el precio total si el hay contenido. Si es mayor que 0 lo mostramos con &&*/}
+        {cart.length > 0 && (
+          <div className="checkout-sidebar">
+            <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continuar pedido</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import initialState from '../initialState';
 import Products from '../components/Products';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
-  return <Products products={initialState.products} />;
+  return (
+    <React.Fragment>
+      <Helmet>
+        <title>Hooks App Merch</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@TU_USER" /> {/** debe existir mi usuario*/}
+        <meta name="twitter:creator" content="@TU_USER" />
+        <meta name="twitter:title" content="Hooks Conf Store" />
+        <meta name="twitter:description" content="Hooks Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:title" content="Hooks Conf Store" />
+        <meta property="og:description" content="Hooks Conf Store" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:url" content="Hooksstore.xyz" />
+        <meta property="og:site_name" content="Hooks Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+      </Helmet>
+      <Products products={initialState.products} />
+    </React.Fragment>
+  );
 };
 
 export default Home;
